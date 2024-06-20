9b8eb990521917ac9e24fd23196ec05c71a1d840
Aidan Sutherland <339501421@GoTVDSB.ca>
2024-06-20 13:18:16 -0400

    working on intro screen

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,13 @@ using System.Reflection.Metadata;
 
 namespace MonoGameFinal___Fallout_Shootout
 {
+    enum Screen
+    {
+        Intro,
+        Main,
+        Controls,
+        Gameover
+    }
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
@@ -21,6 +28,15 @@ namespace MonoGameFinal___Fallout_Shootout
 
         Texture2D paMinigunTexture;
         Rectangle paMinigunRect;
+
+        Texture2D vaultDoorTexture;
+        Rectangle vaultDoorRect;
+
+        Texture2D vaultEntryTexture;
+        Rectangle vaultEntryRect;
+
+        Texture2D vaultBoyTexture;
+        Rectangle vaultBoyRect;
 
         Rectangle rectangleHealthRect;
         Rectangle rectangleAmmoRect;
@@ -47,6 +63,8 @@ namespace MonoGameFinal___Fallout_Shootout
         Random generator;
 
         Color textColor;
+
+        Screen screen;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -65,12 +83,13 @@ namespace MonoGameFinal___Fallout_Shootout
             secondsGun = 0f;
             gunCoolDown = 0.05f;
             secondsEnemy = 0f;
-            spawnCoolDown = 0.5f;
+            spawnCoolDown = 0.05f;
             secondsMoveDelay = 0f;
             moveCoolDown = 0.5f;
             paMinigunRect = new Rectangle(0, 0, 350, 350);
             rectangleHealthRect = new Rectangle(0, 10, 190, 40);
             rectangleAmmoRect = new Rectangle(0, 40, 190, 40);
+            vaultDoorRect = new Rectangle(0, 0, 500, 500);
 
             textColor = Color.Transparent;
 
@@ -78,6 +97,8 @@ namespace MonoGameFinal___Fallout_Shootout
             enemies = new List<Enemy>();
 
             generator = new Random();
+
+            screen = Screen.Intro;
 
             base.Initialize();
             player = new Player(paMinigunTexture, paMinigunRect.X, paMinigunRect.Y);
@@ -94,6 +115,10 @@ namespace MonoGameFinal___Fallout_Shootout
             backgroundTexture = Content.Load<Texture2D>("desert-background");
             eyeBotTexture = Content.Load<Texture2D>("eyebot-pixilart");
             rectangleTexture = Content.Load<Texture2D>("Rectangle");
+            vaultBoyTexture = Content.Load<Texture2D>("final-vaultboy");
+            //vaultDoorTexture = Content.Load<Texture2D>("final-vaultdoor");
+            vaultDoorTexture = Content.Load<Texture2D>("Vault_65");
+            vaultEntryTexture = Content.Load<Texture2D>("final-vaultentry");
 
 
             // TODO: use this.Content to load your game content here
@@ -103,167 +128,185 @@ namespace MonoGameFinal___Fallout_Shootout
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            secondsGun += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            secondsEnemy += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            secondsMoveDelay += (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            // TODO: Add your update logic here
-            keyboardState = Keyboard.GetState();
-            ks1 = Keyboard.GetState();
-
-            prevMouseState = mouseState;
-            mouseState = Mouse.GetState();
-
-            if (keyboardState.IsKeyDown(Keys.Space) ||  mouseState.LeftButton == ButtonState.Pressed)
+            if (screen == Screen.Intro)
             {
-                if (secondsGun >= gunCoolDown && rectangleAmmoRect.Right > 0)
+                mouseState = Mouse.GetState();
+                Window.Title = mouseState.Position + "";
+            }
+            else if (screen == Screen.Main)
+            {
+                
+                secondsGun += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                secondsEnemy += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                secondsMoveDelay += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                // TODO: Add your update logic here
+                keyboardState = Keyboard.GetState();
+                ks1 = Keyboard.GetState();
+
+                prevMouseState = mouseState;
+                mouseState = Mouse.GetState();
+
+                if (keyboardState.IsKeyDown(Keys.Space) || mouseState.LeftButton == ButtonState.Pressed)
                 {
-                    textColor = Color.Transparent;
-                    bullets.Add(new Bullet(bulletTexture, player._location.Center.ToVector2(), mouseState.Position.ToVector2(), 10));
-                    rectangleAmmoRect.X -= 1;
-                    secondsGun = 0;
-                }
-                else if (rectangleAmmoRect.Right <= 0)
-                {
-                    textColor = Color.Black;
-                    if (secondsGun >= 3 || keyboardState.IsKeyDown(Keys.R))
+                    if (secondsGun >= gunCoolDown && rectangleAmmoRect.Right > 0)
                     {
-                        rectangleAmmoRect.X = 0;
                         textColor = Color.Transparent;
+                        bullets.Add(new Bullet(bulletTexture, player._location.Center.ToVector2(), mouseState.Position.ToVector2(), 10));
+                        rectangleAmmoRect.X -= 1;
                         secondsGun = 0;
                     }
-                }
-                
-            }
-            
-            for (int i = 0; i < bullets.Count; i++)
-            {
-                bullets[i].Update();
-
-                if (!window.Intersects(bullets[i].Rect)) // Removes bullets after they leave the window
-                {
-                    bullets.RemoveAt(i);
-                    i--;
-                }
-            }
-            if (secondsEnemy >= 0.5f) // Drawing enemies is broken right now
-            {
-                numRand = generator.Next(0, 3);
-                if (numRand == 0)
-                {
-                    // Top
-                    enemies.Add(new Enemy(eyeBotTexture, generator.Next(0, 100), 0));
-
-                }
-                else if (numRand == 1)
-                {
-                    // Bottom
-                    enemies.Add(new Enemy(eyeBotTexture, generator.Next(0, 100), 100));
-
-                }
-                else if(numRand == 2)
-                {
-                    // Right
-                    enemies.Add(new Enemy(eyeBotTexture, 100, generator.Next(0, 100)));
-
-                }
-                else if (numRand == 3)
-                {
-                    // Left
-                    enemies.Add(new Enemy(eyeBotTexture, 0, generator.Next(0, 100)));
-
-                }
-                secondsEnemy = 0;
-            }
-            List<Enemy> enemiesToRemove = new List<Enemy>();
-            List<Bullet> bulletsToRemove = new List<Bullet>();
-
-            // Check collisions and mark enemies and bullets for removal
-            for (int i = bullets.Count - 1; i >= 0; i--)
-            {
-                Bullet bullet = bullets[i];
-                bool bulletRemoved = false;
-
-                foreach (Enemy enemy in enemies)
-                {
-                    if (enemy.Collide(bullet.Rect))
+                    else if (rectangleAmmoRect.Right <= 0 || keyboardState.IsKeyDown(Keys.R))
                     {
-                        enemy.TakeDamage(1); // Reduce enemy health
-                        bulletsToRemove.Add(bullet); // Mark bullet for removal
-
-                        if (!enemy.IsAlive())
+                        textColor = Color.Black;
+                        if (secondsGun >= 3)
                         {
-                            enemiesToRemove.Add(enemy); // Mark enemy for removal if health <= 0
+                            rectangleAmmoRect.X = 0;
+                            textColor = Color.Transparent;
+                            secondsGun = 0;
                         }
+                    }
 
-                        bulletRemoved = true; // Mark bullet as removed in this iteration
-                        break; // No need to check further enemies for this bullet
+                }
+
+                for (int i = 0; i < bullets.Count; i++)
+                {
+                    bullets[i].Update();
+
+                    if (!window.Intersects(bullets[i].Rect)) // Removes bullets after they leave the window
+                    {
+                        bullets.RemoveAt(i);
+                        i--;
+                    }
+                }
+                if (secondsEnemy >= 0.5f) // Drawing enemies is broken right now
+                {
+                    numRand = generator.Next(0, 3);
+                    if (numRand == 0)
+                    {
+                        // Top
+                        enemies.Add(new Enemy(eyeBotTexture, generator.Next(0, 100), 0));
+
+                    }
+                    else if (numRand == 1)
+                    {
+                        // Bottom
+                        enemies.Add(new Enemy(eyeBotTexture, generator.Next(0, 100), 100));
+
+                    }
+                    else if (numRand == 2)
+                    {
+                        // Right
+                        enemies.Add(new Enemy(eyeBotTexture, 100, generator.Next(0, 100)));
+
+                    }
+                    else if (numRand == 3)
+                    {
+                        // Left
+                        enemies.Add(new Enemy(eyeBotTexture, 0, generator.Next(0, 100)));
+
+                    }
+                    secondsEnemy = 0;
+                }
+                List<Enemy> enemiesToRemove = new List<Enemy>();
+                List<Bullet> bulletsToRemove = new List<Bullet>();
+
+                // Check collisions and mark enemies and bullets for removal
+                for (int i = bullets.Count - 1; i >= 0; i--)
+                {
+                    Bullet bullet = bullets[i];
+                    bool bulletRemoved = false;
+
+                    foreach (Enemy enemy in enemies)
+                    {
+                        if (enemy.Collide(bullet.Rect))
+                        {
+                            enemy.TakeDamage(1); // Reduce enemy health
+                            bulletsToRemove.Add(bullet); // Mark bullet for removal
+
+                            if (!enemy.IsAlive())
+                            {
+                                enemiesToRemove.Add(enemy); // Mark enemy for removal if health <= 0
+                            }
+
+                            bulletRemoved = true; // Mark bullet as removed in this iteration
+                            break; // No need to check further enemies for this bullet
+                        }
+                    }
+
+                    // Remove bullet if it collided with any enemy
+                    if (bulletRemoved)
+                    {
+                        bullets.RemoveAt(i);
                     }
                 }
 
-                // Remove bullet if it collided with any enemy
-                if (bulletRemoved)
+                // Remove enemies marked for removal
+                foreach (Enemy enemyToRemove in enemiesToRemove)
                 {
-                    bullets.RemoveAt(i);
+                    enemies.Remove(enemyToRemove);
                 }
-            }
 
-            // Remove enemies marked for removal
-            foreach (Enemy enemyToRemove in enemiesToRemove)
-            {
-                enemies.Remove(enemyToRemove);
-            }
+                // Remove bullets marked for removal
+                foreach (Bullet bulletToRemove in bulletsToRemove)
+                {
+                    bullets.Remove(bulletToRemove);
+                }
 
-            // Remove bullets marked for removal
-            foreach (Bullet bulletToRemove in bulletsToRemove)
-            {
-                bullets.Remove(bulletToRemove);
-            }
+                // Update remaining enemies
+                foreach (Enemy enemy in enemies)
+                {
+                    // Recalculate enemy speed
+                    if (secondsMoveDelay >= moveCoolDown)
+                    {
+                        enemy.Move(player);
+                    }
 
-            // Update remaining enemies
-            foreach (Enemy enemy in enemies)
-            {
-                // Recalculate enemy speed
+                    enemy.Update();
+                }
                 if (secondsMoveDelay >= moveCoolDown)
+                    secondsMoveDelay = 0;
+
+                player.HSpeed = 0;
+                player.VSpeed = 0;
+
+                if (keyboardState.IsKeyDown(Keys.D) || (keyboardState.IsKeyDown(Keys.Right)))
+                    player.HSpeed = 2;
+                else if (keyboardState.IsKeyDown(Keys.A) || (keyboardState.IsKeyDown(Keys.Left)))
+                    player.HSpeed = -2;
+
+                if (keyboardState.IsKeyDown(Keys.W) || (keyboardState.IsKeyDown(Keys.Up)))
+                    player.VSpeed = -2;
+                else if (keyboardState.IsKeyDown(Keys.S) || (keyboardState.IsKeyDown(Keys.Down)))
+                    player.VSpeed = 2;
+
+                if (keyboardState.IsKeyDown(Keys.LeftShift) || (keyboardState.IsKeyDown(Keys.RightShift)))
                 {
-                    enemy.Move(player);
+                    player.VSpeed *= 1.8f;
+                    player.HSpeed *= 1.8f;
                 }
 
-                enemy.Update();
+                player.Update(gameTime);
+                rectangleHealthRect.X -= 0;
+
+                if (rectangleHealthRect.Right < 0)
+                {
+                    Exit();
+                }
+                Window.Title = mouseState.Position + "";
+
+                base.Update(gameTime);
             }
-            if (secondsMoveDelay >= moveCoolDown)
-                secondsMoveDelay = 0;
-
-            player.HSpeed = 0;
-            player.VSpeed = 0;
-
-            if (keyboardState.IsKeyDown(Keys.D) || (keyboardState.IsKeyDown(Keys.Right)))
-                player.HSpeed = 2;
-            else if (keyboardState.IsKeyDown(Keys.A) || (keyboardState.IsKeyDown(Keys.Left)))
-                player.HSpeed = -2;
-
-            if (keyboardState.IsKeyDown(Keys.W) || (keyboardState.IsKeyDown(Keys.Up)))
-                player.VSpeed = -2;
-            else if (keyboardState.IsKeyDown(Keys.S) || (keyboardState.IsKeyDown(Keys.Down)))
-                player.VSpeed = 2;
-
-            if (keyboardState.IsKeyDown(Keys.LeftShift) || (keyboardState.IsKeyDown(Keys.RightShift)))
+            else if (screen == Screen.Controls)
             {
-                player.VSpeed *= 1.8f;
-                player.HSpeed *= 1.8f;
+
             }
-
-            player.Update(gameTime);
-            rectangleHealthRect.X -= 0;
-
-            if (rectangleHealthRect.Right < 0)
+            else if (screen == Screen.Gameover)
             {
-                Exit();
-            }
-            Window.Title = mouseState.Position + "";
 
-            base.Update(gameTime);
+            }
         }
+            
         
 
         protected override void Draw(GameTime gameTime)
@@ -272,32 +315,48 @@ namespace MonoGameFinal___Fallout_Shootout
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw(backgroundTexture, window, Color.White);
-
-            _spriteBatch.Draw(rectangleTexture, new Rectangle(0, 5, 200, 50), Color.White);
-            _spriteBatch.Draw(rectangleTexture, rectangleHealthRect, Color.Green);
-            _spriteBatch.Draw(rectangleTexture, new Rectangle(0, 35, 200, 50), Color.White);
-            _spriteBatch.Draw(rectangleTexture, rectangleAmmoRect, Color.Goldenrod);
-
-            _spriteBatch.DrawString(overseerFontUI, "Reloading", new Vector2(player._location.X, player._location.Top), textColor);
-
-            player.Draw(_spriteBatch);
-            foreach (Enemy enemy in enemies)
+            if (screen == Screen.Intro)
             {
-                enemy.Draw(_spriteBatch); Rectangle healthBarRect = new Rectangle(enemy._location.X, enemy._location.Y - 10, enemy._location.Left, 5);
-                float healthPercentage = (float)enemy.Health / enemy.MaxHealth; // Calculate percentage of health remaining
-
-                // Draw health bar background (gray or red)
-                _spriteBatch.Draw(rectangleTexture, healthBarRect, Color.Gray);
-
-                // Draw health bar foreground (green or red, indicating remaining health)
-                Rectangle healthBarFillRect = new Rectangle(healthBarRect.Left, healthBarRect.Top, (int)(healthBarRect.Width * healthPercentage), healthBarRect.Height);
-                _spriteBatch.Draw(rectangleTexture, healthBarFillRect, Color.Green);
+                _spriteBatch.Draw(vaultDoorTexture, new Rectangle(180, 115, vaultDoorRect.Width, vaultDoorRect.Height), Color.White);
+                _spriteBatch.Draw(vaultEntryTexture, window, Color.White);
             }
-
-            foreach (Bullet bullet in bullets)
+            else if (screen == Screen.Main)
             {
-                bullet.Draw(_spriteBatch);
+                _spriteBatch.Draw(backgroundTexture, window, Color.White);
+
+                _spriteBatch.Draw(rectangleTexture, new Rectangle(0, 5, 200, 50), Color.White);
+                _spriteBatch.Draw(rectangleTexture, rectangleHealthRect, Color.Green);
+                _spriteBatch.Draw(rectangleTexture, new Rectangle(0, 35, 200, 50), Color.White);
+                _spriteBatch.Draw(rectangleTexture, rectangleAmmoRect, Color.Goldenrod);
+
+                _spriteBatch.DrawString(overseerFontUI, "Reloading", new Vector2(player._location.X, player._location.Top), textColor);
+
+                player.Draw(_spriteBatch);
+                foreach (Enemy enemy in enemies)
+                {
+                    enemy.Draw(_spriteBatch); Rectangle healthBarRect = new Rectangle(enemy._location.X, enemy._location.Y - 10, enemy._location.Right, 5);
+                    float healthPercentage = (float)enemy.Health / enemy.MaxHealth; // Calculate percentage of health remaining
+
+                    // Draw health bar background (gray or red)
+                    _spriteBatch.Draw(rectangleTexture, healthBarRect, Color.Gray);
+
+                    // Draw health bar foreground (green or red, indicating remaining health)
+                    Rectangle healthBarFillRect = new Rectangle(healthBarRect.Left, healthBarRect.Bottom, (int)(healthBarRect.Width * healthPercentage), healthBarRect.Height);
+                    _spriteBatch.Draw(rectangleTexture, healthBarFillRect, Color.Green);
+                }
+
+                foreach (Bullet bullet in bullets)
+                {
+                    bullet.Draw(_spriteBatch);
+                }
+            }
+            else if (screen == Screen.Controls)
+            {
+
+            }
+            else if (screen == Screen.Gameover)
+            {
+
             }
 
             _spriteBatch.End();
